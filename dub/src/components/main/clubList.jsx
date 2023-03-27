2cd6fabb80d00455f20609213f0d1b9e5f3d3631
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2023-03-27 22:31:24 +0900

    feat: api 입력 준비

--- dub/src/components/main/clubList.jsx
+++ dub/src/components/main/clubList.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
 import clubdata from "../../dummy/clubdata.json";
+// import { url } from '../../url';
 import './clubList.scss'
 function ClubList() {
   const data = clubdata;
-  const [clubs, setClubs] = useState([]);
+  // const [clubs, setClubs] = useState([]);
+
+  // useEffect(() => {
+  //   const getClub = async () => {
+  //     await axios.get(
+  //       `${url}/app/post/getAll`
+  //     ).then(response => {
+  //       setClubs(response.data.result)
+  //     })
+  //   }
+  //   getClub();
+  // }, []);
   return(
     <div className='Club-container'>
       <ul className='Club-list'>
