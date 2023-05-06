1b2c34bb9cf537635960ea87adbe1c8a7a801bfe
ksweeni <ksween@naver.com>
2023-05-06 21:47:00 +0900

    clubList styling
    clubList 페이지 틀 수정 작업 시작

--- dub/src/components/detailInput/detailInput.jsx
+++ dub/src/components/detailInput/detailInput.jsx
@@ -2,108 +2,58 @@ import React from 'react';
 import { useState } from 'react';
 //import '../sass/Button.scss';
 import './detailInput.scss';
-import { useNavigate } from 'react-router-dom';
-import imageCompression from 'browser-image-compression';
-import { useSelector } from 'react-redux';
-import axios from 'axios';
-import { url } from '../../Url';
+//import Button from '@mui/material/Button';
+// import axios from 'axios';
 function DetailInput() {
+  const [state, setState] = useState({
+    image:"",
+    title:"",
+    clubName:"",
+    content:"",
+    category:""
+  });
 
-  const [name, setName] = useState('');
-  const [category, setCategory] = useState('');
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  
+
   const [imageSrc, setImageSrc] = useState('');
-  const [image, setImage] = useState(null);
 
-  const navigate = useNavigate();
-  const token = useSelector((state) => state.reducer.token); 
+  const encodeFileToBase64 = (fileBlob) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(fileBlob);
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        setImageSrc(reader.result);
+        resolve();
+      };
+    });
+  };
 
-  const imageResize = async(e) => {
-    let file = e.target.files[0];
-    setImage(file);
-    const option = {
-      maxSizeMB: 2,
-      maxWidthOrHeight: 150,
-    }
+  const handleChangeState = (e) => {
+    console.log(e.target.name);
+    console.log(e.target.value);
 
-    try {
-      const compressedFile = await imageCompression(file, option);
-      const promise = imageCompression.getDataUrlFromFile(compressedFile);
-      promise.then(result => {
-        setImageSrc(result);
-      })
-    } catch (error) {
-      console.log(error);
-      alert("글 작성 실패");
-    }
-  }
-
-  const handleName = (e) => {
-    e.preventDefault();
-    setName(e.target.value);
-  }
-  const handleCategory = (e) => {
-    e.preventDefault();
-    setCategory(e.target.value);
-  }
-  const handleTitle = (e) => {
-    e.preventDefault();
-    setTitle(e.target.value);
-  }
-  const handleContent = (e) => {
-    e.preventDefault();
-    setContent(e.target.value);
-  }
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const handleSubmit = (e) => {
-
-    e.preventDefault();
-    const submitData = {
-      'title' : title,
-      'content' : content,
-      'category' : category,
-    };
-    console.log(submitData);
-    const formData = new FormData();
-
-    formData.append('json', submitData);
-    formData.append('images', image);
-    // FormData의 key 확인
-    for (const key of formData.keys()) {
-      console.log(key);
-    }
-    // FormData의 value 확인
-    for (const value of formData.values()) {
-      console.log(value);
-    }
-    console.log(formData);
-    try {
-      axios.post(
-        `${url}/app/post/write-post`,
-        formData,
-        {
-          headers: {
-            Authorization : token,
-            'Content-Type': 'multipart/form-data',
-          },
-          transformRequest: formData => formData,
-        },
-      ).then(response => {
-        alert(response.result);
-        navigate('/')
-      })
-    } catch(error) {
-      console.log(error.response);
-      alert(error);
-    }
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+      image:imageSrc,
+    })
+    console.log(state);
+    
+    alert("저장 성공");
   }
 
   return(
     <div className="detailInput">
-      <div className="detailheader">
+      <header className="detailheader">
           <span>동아리 모집 공고 작성</span>
-      </div>
+      </header>
 
     <form className='editor'
           onSubmit={(e) => handleSubmit(e)}>
@@ -113,19 +63,19 @@ function DetailInput() {
 
       <div className='detailbodyheader'>동아리 이름은 무엇인가요?</div>
           <p className='detailbodyguide'>• 동아리 이름을 작성해주세요</p>
-          <textarea className='textarea' name="clubName" placeholder='clubname' value={name} onChange={handleName}/>
+          <textarea className='textarea' name="clubName" placeholder='clubname' value={state.clubName} onChange={handleChangeState}/>
       
       <div className='detailbodyheader'>동아리 분과를 입력해주세요</div>
         <p className='detailbodyguide'>• 분과명은 체육/예술 .. </p>
-      <textarea className='textarea' name="category" placeholder='category' value={category}
-        onChange={handleCategory}
+      <textarea className='textarea' name="category" placeholder='category' value={state.category}
+        onChange={handleChangeState}
         />
       
       <div>
           <div className='detailbodyheader'>공고 제목을 작성해주세요</div>
           <p className='detailbodyguide'>• 모든 학우들의 눈을 사로잡을만한 제목을 작성해주세요</p>
-      <textarea className='textarea' name="title" placeholder='title' value={title} 
-        onChange={handleTitle}
+      <textarea className='textarea' name="title" placeholder='title' value={state.title} 
+        onChange={handleChangeState}
         />
       </div>
       <div>
@@ -134,9 +84,9 @@ function DetailInput() {
       <input type="file"
             name="image" 
             placeholder='image/jpg/png/jpeg'
-            accept="image/png, image/jpeg"
+            accept="image/jpg, image/png, image/jpeg"
             onChange={(e) => {
-              imageResize(e);
+              encodeFileToBase64(e.target.files[0]);
             }}
         />
       <div className='preview'>
@@ -150,8 +100,8 @@ function DetailInput() {
       <textarea className='textarea' name="content" placeholder='textarea' autoSize={{
           minRows: 5,
           maxRows: 100,
-        }} value={content} 
-        onChange={handleContent}
+        }} value={state.content} 
+        onChange={handleChangeState}
         />
       </div>
       </div>
